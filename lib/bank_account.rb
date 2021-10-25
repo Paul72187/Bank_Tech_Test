cb9d8f88e570bf25ce1bd39e002854b644f5dca2
Paul Cooper <paul_cooper_pc@hotmail.com>
2021-10-25 15:37:53 +0100

    Deposit method added

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
+require_relative 'transaction'
+
 class BankAccount
   def initialize
     @transactions = []
+  end
+
+  def deposit(amount)
+    @transactions << Transaction.new.create(:deposit, amount)
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require 'bank_account'
+require_relative 'bank_account'
 
-describe BankAccount do
+describe 'bank account' do
   before(:each) do
     allow(Date).to receive(:today).and_return('2023-01-10')
     allow(Transaction).to receive(:new).and_return(transaction)
   end
 end
 
-describe Deposit do
+describe 'deposit' do
   it 'takes deposits in pounds and pence to two decimal places' do
     allow(transaction).to receive(:create).and_return({ type: :deposit, date: '10/01/2013', amount: 20.00 })
     expect { subject.deposit(20.00) }.not_to raise_exception
