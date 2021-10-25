4177afbc38b0f6bcf6ee5403fbe6a71cbb380538
Paul Cooper <paul_cooper_pc@hotmail.com>
2021-10-25 15:14:38 +0100

    Bank Account class added

---
+++ lib/bank_account.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class BankAccount
+  def initialize
+    @transactions = []
+  end
+end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,7 @@
 # frozen_literal_string: true
 
+require 'bank_account'
+
 describe BankAccount do
   before(:each) do
     allow(Date).to receive(:today).and_return('2023-01-10')
