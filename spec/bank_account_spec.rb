5e3cb750b76a3c2ae4056da989dc9a2e92887c56
Paul Cooper <paul_cooper_pc@hotmail.com>
2021-10-25 15:21:14 +0100

    Deposit test added

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,4 +1,4 @@
-# frozen_literal_string: true
+# frozen_string_literal: true
 
 require 'bank_account'
 
@@ -6,5 +6,12 @@ describe BankAccount do
   before(:each) do
     allow(Date).to receive(:today).and_return('2023-01-10')
     allow(Transaction).to receive(:new).and_return(transaction)
+  end
+end
+
+describe Deposit do
+  it 'takes deposits in pounds and pence to two decimal places' do
+    allow(transaction).to receive(:create).and_return({ type: :deposit, date: '10/01/2013', amount: 20.00 })
+    expect { subject.deposit(20.00) }.not_to raise_exception
   end
 end
